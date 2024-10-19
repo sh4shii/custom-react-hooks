1fe4f0cf04b4a673a55293024712f00cba37c7cb
Shashi Kumar <shashisaurav50@gmail.com>
2024-10-19 14:13:16 +0530

    useToast hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ToastProvider } from "./hooks/useToast";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <ToastProvider>
     <App />
-  </StrictMode>,
-)
+  </ToastProvider>
+);
