ce42abb24eeb0cb8a917a4c83a1afd6662a78c28
Shashi Kumar <shashisaurav50@gmail.com>
2024-10-21 23:41:43 +0530

    useDebounce hook

---
+++ src/hooks/useDebounce.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cleanup function to clear the timeout if value or delay changes
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
